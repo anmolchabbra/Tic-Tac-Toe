74b24a36ca8f6476b372d47d58a1690528608341
Anmol Chabbra <46331514+anmolchabbra@users.noreply.github.com>
2023-10-31 17:16:19 -0400

    "Created a board  where player can click on the items and items would set to "X" or "O" in alternate turns.

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './style.css'
+import Board from './App.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Board />
+  </React.StrictMode>,
+)
